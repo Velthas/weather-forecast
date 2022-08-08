aa47a968860515fbdac39a56014d7909ab460b85
Velthas <damianobattaglia97@gmail.com>
2022-08-08 19:13:27 +0200

    Webpack setup complete

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(jpg|jpeg|png|bmp|gif|svg)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
